9a9016811a5fb9ce5f6272b5c4b9306035f244fa
thiyagu3965 <2k17ece113@kiot.ac.in>
2021-05-31 09:34:39 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -6,10 +6,26 @@ function App()
 
   return (
 <>
-    <div className="App">
-      <h1>hai from designs spot</h1>
-   
-    </div>
+<section>
+        <div classNameName="card">
+            <div className="face">
+                <h3>Designs Spot</h3>
+                <h6>mobile</h6>
+                <h6> 9344553066 </h6>
+                
+                <div className="icon-content">
+                    <div className="centy" >
+                        <i className="fa fa-camera-retro"></i>
+                        <h5>Our Gallery</h5>
+                    </div>
+                   
+                </div>
+                <div className="slide-content" >
+                    <a target="_blank" href="https://wa.me/9344553066"><h5 >Click to <i className="fa fa-whatsapp"> Whatsapp ></i></h5></a>
+                </div>        
+            </div>
+        </div>
+    </section>
     </>
   );
 }
