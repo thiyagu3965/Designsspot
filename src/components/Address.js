01cf58493049041c17756a32f3eef4740c9e197c
thiyagu3965 <2k17ece113@kiot.ac.in>
2021-05-31 22:55:39 -0700

    changes

--- src/components/Address.js
+++ src/components/Address.js
@@ -5,8 +5,8 @@ export default function Address()
     return (
         <div id="section3" className="container-fluid common section3" >
           <h1 className="last"><u>DesignsSpot</u></h1>
-          <p className="last">Trichy Main road <br/>Opp to ICICI Bank  <br/> Salem - 636002</p>
-          
+          <p className="last">Trichy Main road <br/>Opposite to ICICI Bank  <br/> Salem - 636002</p>
+ 
         </div>
        
     )
--- src/components/Section1-section3.js
+++ src/components/Section1-section3.js
@@ -3,9 +3,15 @@ import React from 'react'
 export default function section() {
     return (
         <div id="section1" className="container-fluid bg-dark common" >
-        <h1 className="">Section 1</h1>
-        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        <h1 className="">Designs Spot</h1>
+        <button type="button" className="btn btn-dark magikbtn" data-toggle="collapse" data-target="#demo">Our-Motto</button>
+  <div id="demo" className="collapse btntxt">
+  <p>Flex Editing And Printing..<br />We Design What U Think</p>
+  </div>
+
+
+        
+    
       </div>
 
       //sec 2 gallery
