907e640d3c8d2f2281b9ea44441bb0f7658bee11
thiyagu3965 <2k17ece113@kiot.ac.in>
2021-06-01 00:45:24 -0700

    changes

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,11 +2,113 @@ import React from 'react'
 
 export default function Gallery() {
     return (
-        <div id="section2" className="container-fluid common" >
-          <h1 className="">Gallery</h1>
-          <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-          <p>Try to scroll this sectio  n and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+<div id="section2" className="container-fluid common" >
+  <h2 className="">Gallery</h2>            
+  <button type="button" class="btn btn-primary magikbtn" data-toggle="collapse" data-target="#gallery">Click to Show Gallery</button>
+  <div id="gallery" class="collapse gallery">
+    <div class="row">
+        <div class="col-3"> 
+          <div class="card" >
+            <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
+              <div class="card-body">
+                <h4 class="card-title ">Flex1</h4>
+                <p class="card-text">Some example text.</p>
+              </div>
+          </div>
         </div>
+        <div class="col-3"> 
+          <div class="card" >
+            <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
+              <div class="card-body">
+                <h4 class="card-title ">Flex1</h4>
+                <p class="card-text">Some example text.</p>
+              </div>
+          </div>
+        </div>
+        <div class="col-3"> 
+          <div class="card" >
+            <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
+              <div class="card-body">
+                <h4 class="card-title ">Flex1</h4>
+                <p class="card-text">Some example text.</p>
+              </div>
+          </div>
+        </div>
+        <div class="col-3"> 
+          <div class="card" >
+            <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
+              <div class="card-body">
+                <h4 class="card-title ">Flex1</h4>
+                <p class="card-text">Some example text.</p>
+              </div>
+          </div>
+        </div>
+        <div class="col-3"> 
+          <div class="card" >
+            <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
+              <div class="card-body">
+                <h4 class="card-title ">Flex1</h4>
+                <p class="card-text">Some example text.</p>
+              </div>
+          </div>
+        </div>
+        <div class="col-3"> 
+          <div class="card" >
+            <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
+              <div class="card-body">
+                <h4 class="card-title ">Flex1</h4>
+                <p class="card-text">Some example text.</p>
+              </div>
+          </div>
+        </div>
+        <div class="col-3"> 
+          <div class="card" >
+            <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
+              <div class="card-body">
+                <h4 class="card-title ">Flex1</h4>
+                <p class="card-text">Some example text.</p>
+              </div>
+          </div>
+        </div>
+        <div class="col-3"> 
+          <div class="card" >
+            <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
+              <div class="card-body">
+                <h4 class="card-title ">Flex1</h4>
+                <p class="card-text">Some example text.</p>
+              </div>
+          </div>
+        </div>
+        <div class="col-3"> 
+          <div class="card" >
+            <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
+              <div class="card-body">
+                <h4 class="card-title ">Flex1</h4>
+                <p class="card-text">Some example text.</p>
+              </div>
+          </div>
+        </div>
+        <div class="col-3"> 
+          <div class="card" >
+            <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
+              <div class="card-body">
+                <h4 class="card-title ">Flex1</h4>
+                <p class="card-text">Some example text.</p>
+              </div>
+          </div>
+        </div>
+        <div class="col-3"> 
+          <div class="card" >
+            <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
+              <div class="card-body">
+                <h4 class="card-title ">Flex1</h4>
+                <p class="card-text">Some example text.</p>
+              </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</div>
         
-    )
+)
 }
