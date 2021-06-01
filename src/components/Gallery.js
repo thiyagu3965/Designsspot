ff6b7d0055b43e08b78465b3da5f23e61c8d6434
thiyagu3965 <2k17ece113@kiot.ac.in>
2021-06-01 00:52:05 -0700

    changes

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,105 +7,98 @@ export default function Gallery() {
   <button type="button" class="btn btn-primary magikbtn" data-toggle="collapse" data-target="#gallery">Click to Show Gallery</button>
   <div id="gallery" class="collapse gallery">
     <div class="row">
-        <div class="col-3"> 
+      {/* card */}
+        <div class="col-lg-3 col-sm-4 col-xs-6 col-lg-2 "> 
           <div class="card" >
             <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
               <div class="card-body">
-                <h4 class="card-title ">Flex1</h4>
+                <p class="card-title ">Flex1</p>
                 <p class="card-text">Some example text.</p>
               </div>
           </div>
         </div>
-        <div class="col-3"> 
+        <div class="col-lg-3 col-sm-4 col-xs-6 col-lg-2 "> 
           <div class="card" >
             <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
               <div class="card-body">
-                <h4 class="card-title ">Flex1</h4>
+                <p class="card-title ">Flex1</p>
                 <p class="card-text">Some example text.</p>
               </div>
           </div>
         </div>
-        <div class="col-3"> 
+        <div class="col-lg-3 col-sm-4 col-xs-6 col-lg-2 "> 
           <div class="card" >
             <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
               <div class="card-body">
-                <h4 class="card-title ">Flex1</h4>
+                <p class="card-title ">Flex1</p>
                 <p class="card-text">Some example text.</p>
               </div>
           </div>
         </div>
-        <div class="col-3"> 
+        <div class="col-lg-3 col-sm-4 col-xs-6 col-lg-2 "> 
           <div class="card" >
             <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
               <div class="card-body">
-                <h4 class="card-title ">Flex1</h4>
+                <p class="card-title ">Flex1</p>
                 <p class="card-text">Some example text.</p>
               </div>
           </div>
         </div>
-        <div class="col-3"> 
+        <div class="col-lg-3 col-sm-4 col-xs-6 col-lg-2 "> 
           <div class="card" >
             <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
               <div class="card-body">
-                <h4 class="card-title ">Flex1</h4>
+                <p class="card-title ">Flex1</p>
                 <p class="card-text">Some example text.</p>
               </div>
           </div>
         </div>
-        <div class="col-3"> 
+        <div class="col-lg-3 col-sm-4 col-xs-6 col-lg-2 "> 
           <div class="card" >
             <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
               <div class="card-body">
-                <h4 class="card-title ">Flex1</h4>
+                <p class="card-title ">Flex1</p>
                 <p class="card-text">Some example text.</p>
               </div>
           </div>
         </div>
-        <div class="col-3"> 
+        <div class="col-lg-3 col-sm-4 col-xs-6 col-lg-2 "> 
           <div class="card" >
             <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
               <div class="card-body">
-                <h4 class="card-title ">Flex1</h4>
+                <p class="card-title ">Flex1</p>
                 <p class="card-text">Some example text.</p>
               </div>
           </div>
         </div>
-        <div class="col-3"> 
+        <div class="col-lg-3 col-sm-4 col-xs-6 col-lg-2 "> 
           <div class="card" >
             <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
               <div class="card-body">
-                <h4 class="card-title ">Flex1</h4>
+                <p class="card-title ">Flex1</p>
                 <p class="card-text">Some example text.</p>
               </div>
           </div>
         </div>
-        <div class="col-3"> 
+        <div class="col-lg-3 col-sm-4 col-xs-6 col-lg-2 "> 
           <div class="card" >
             <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
               <div class="card-body">
-                <h4 class="card-title ">Flex1</h4>
+                <p class="card-title ">Flex1</p>
                 <p class="card-text">Some example text.</p>
               </div>
           </div>
         </div>
-        <div class="col-3"> 
+        <div class="col-lg-3 col-sm-4 col-xs-6 col-lg-2 "> 
           <div class="card" >
             <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
               <div class="card-body">
-                <h4 class="card-title ">Flex1</h4>
+                <p class="card-title ">Flex1</p>
                 <p class="card-text">Some example text.</p>
               </div>
           </div>
         </div>
-        <div class="col-3"> 
-          <div class="card" >
-            <img class="card-img-top" src="https://picsum.photos/id/180/2400/1600" alt="Card image" />
-              <div class="card-body">
-                <h4 class="card-title ">Flex1</h4>
-                <p class="card-text">Some example text.</p>
-              </div>
-          </div>
-        </div>
+      {/* card end */}
     </div>
   </div>
 </div>
