d5fae795bfba48f0994cf80167942090b814e737
thiyagu3965 <2k17ece113@kiot.ac.in>
2021-05-31 21:45:51 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -33,8 +33,7 @@ function App()
         <div id="section3" className="container-fluid common" >
           <h1 className="last"><u>DesignsSpot</u></h1>
           <p className="last">Trichy Main road <br/> Salem - 636002</p>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d976.9093569254795!2d78.15609452915687!3d11.649164612964718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3babf1b9c642e1eb%3A0x621811a9fcfe27a5!2sSri%20Sakthivel%20Tex%20Tiles%20Stores!5e0!3m2!1sen!2sin!4v1622520378064!5m2!1sen!2sin" width="600" height="450" allowfullscreen="" loading="lazy"></iframe>
-
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d807.7312816620281!2d78.15610372915687!3d11.64907461296514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTHCsDM4JzU2LjciTiA3OMKwMDknMjMuOSJF!5e1!3m2!1sen!2sin!4v1622522632243!5m2!1sen!2sin" width="600" height="450" allowfullscreen="" loading="lazy"></iframe>
         </div>
  </>   
   );
