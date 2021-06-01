70adbf3ec58ca95d42c19636673d1c76a4617250
thiyagu3965 <2k17ece113@kiot.ac.in>
2021-05-31 22:35:53 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Section from './components/Section1-section3'
 import Gallery from './components/Gallery'
+import Address from './components/Address'
 
 function App() 
 {
@@ -13,6 +14,7 @@ function App()
 
        <Section />
        <Gallery/>
+       <Address />
        <div className="map">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d807.7312816620281!2d78.15610372915687!3d11.64907461296514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTHCsDM4JzU2LjciTiA3OMKwMDknMjMuOSJF!5e1!3m2!1sen!2sin!4v1622522632243!5m2!1sen!2sin" width="600" height="450" allowfullscreen="" loading="lazy" >
             </iframe>
--- src/components/Address.js
+++ src/components/Address.js
@@ -5,7 +5,7 @@ export default function Address()
     return (
         <div id="section3" className="container-fluid common section3" >
           <h1 className="last"><u>DesignsSpot</u></h1>
-          <p className="last">Trichy Main road <br/> Salem - 636002</p>
+          <p className="last">Trichy Main road <br/>Opp to ICICI Bank  <br/> Salem - 636002</p>
           
         </div>
        
